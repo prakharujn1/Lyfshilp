8c33021322ee73ce53473002946d6cc012c03388
prakharujn1 <128589011+prakharujn1@users.noreply.github.com>
2025-05-31 08:39:06 +0530

    added login route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
@@ -6,7 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 const userRoutes = require("./routes/userRoutes");
 
+// Enable CORS for all origins
+app.use(cors());
+
 app.use(express.json());
 app.use("/", userRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
