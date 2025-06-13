56f933dd3a16ffd19f3b2691d84e685ab4d74322
YeahitsDivyansh <divyansh972003@gmail.com>
2025-06-13 10:52:31 +0530

    Initialized new Level 2 social-emotional learning games: StressBusterLab, ConflictQuest, and MindBodyMatchUp

---
+++ client/src/components/SEL/Level-2/MindBodyMatchUp/MindBodyMatchUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MindBodyMatchUp = () => {
+  return <div>MindBodyMatchUp</div>;
+};
+
+export default MindBodyMatchUp;
---
+++ client/src/components/SEL/Level-2/StressBusterLab/StressBusterLab.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StressBusterLab = () => {
+  return <div>StressBusterLab</div>;
+};
+
+export default StressBusterLab;
